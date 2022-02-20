ab423374bdff30f412808e060b112f3f2b3c2f07
Mendy <mendyw94@gmail.com>
2022-02-20 00:44:01 -0500

    commit tree c9aa0ee4008d63bedf9acdbf2a8407878c3bd6dd

--- index.js
+++ index.js
@@ -10,7 +10,20 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
+// "x" repersents the strings in the array, and the "i" repersents the chars in the strings.
 const titleCased = () => {
-  return tutorials
+  tutorials.forEach((tutorial, index) => {
+  const splitSentence = tutorial.split(" ");
+  const alteredSentence = splitSentence.map(word => {
+      const charecters = word.split("");
+      const firstChar = charecters.shift();
+      // debugger
+      return firstChar.toUpperCase() + charecters.join("");
+      //  debugger
+      }).join(" ")
+      //  debugger
+      tutorials[index] = alteredSentence;
+  })
+  return tutorials;
 }
+titleCased()
